a2b93ba0174ccf78d07c1d5b5be5ee9ace6c2f22
Felipe Brito <felipebrito0429@gmail.com>
2026-03-28 03:23:43 -0300

    add FaceDetectionService with Haar Cascade face detection

---
+++ src/main/java/com/felipebrito/facial_detection/services/FaceDetectionService.java
@@ -0,0 +1,37 @@
+package com.felipebrito.facial_detection.services;
+
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FaceDetectionService {
+    private CascadeClassifier faceDetector;
+
+    public FaceDetectionService(){
+        var resource = getClass().getClassLoader().getResource("models/haarcascade_frontalface_default.xml");
+        if (resource == null){
+            throw new RuntimeException("Haarcascade file not found");
+        }
+        String path = resource.getPath();
+        this.faceDetector = new CascadeClassifier(path);
+        boolean test = faceDetector.empty();
+        if(test) {
+            throw new RuntimeException("Error loading Haar Cascade");
+        }
+    }
+    public Mat detectAndDraw(Mat frame){
+        Mat gray = new Mat();
+        Imgproc.cvtColor(frame, gray, Imgproc.COLOR_BGR2GRAY);
+        MatOfRect faces = new MatOfRect();
+        faceDetector.detectMultiScale(gray, faces);
+        for(Rect rect : faces.toArray()){
+            Imgproc.rectangle(frame, new Point(rect.x, rect.y), new Point(rect.x + rect.width,
+                            rect.y + rect.height),
+                            new Scalar(0, 255, 0), 2);
+        }
+        return frame;
+    }
+
+}
---
+++ src/test/java/com/felipebrito/facial_detection/FacialDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipebrito.facial_detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacialDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
