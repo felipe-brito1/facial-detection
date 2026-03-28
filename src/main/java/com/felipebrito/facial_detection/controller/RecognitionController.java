d91aebf7bd168f4e45adfa7680506249c4738036
Felipe Brito <felipebrito0429@gmail.com>
2026-03-28 20:04:18 -0300

    add RecognitionController with train endpoint

---
+++ src/main/java/com/felipebrito/facial_detection/controller/RecognitionController.java
@@ -0,0 +1,25 @@
+package com.felipebrito.facial_detection.controller;
+
+import com.felipebrito.facial_detection.services.FaceRecognitionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/recognition")
+public class RecognitionController {
+    private FaceRecognitionService faceRecognitionService;
+
+    public RecognitionController(FaceRecognitionService faceRecognitionService){
+        this.faceRecognitionService = faceRecognitionService;
+    }
+
+    @PostMapping("/train")
+    ResponseEntity<String> train(){
+        faceRecognitionService.train();
+        return ResponseEntity.ok("Model trained sucessfully");
+    }
+
+
+}
